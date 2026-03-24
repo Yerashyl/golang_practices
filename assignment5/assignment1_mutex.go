52b850fac8020c139455d62e6b89c69887f44352
Yerashyl <erasylggggggg@gmail.com>
2026-03-24 17:45:02 +0500

    fifth

---
+++ assignment5/assignment1_mutex.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+	var mu sync.RWMutex
+	safeMap := make(map[string]int)
+
+	var wg sync.WaitGroup
+	for i := 0; i < 100; i++ {
+		wg.Add(1)
+		go func(key int) {
+			defer wg.Done()
+			mu.Lock()
+			safeMap["key"] = key
+			mu.Unlock()
+		}(i)
+	}
+
+	wg.Wait()
+
+	mu.RLock()
+	value := safeMap["key"]
+	mu.RUnlock()
+	fmt.Printf("Value: %d\n", value)
+}
